25a5d3d7542b4dafc2575ceb7490e8ae2ecd4e45
hgj316@gmail.com <hgj316@gmail.com@b9641ce2-5edb-6cd8-cb65-f675c16494b0>
2012-02-28 11:10:10 +0000

    v0.2 - ConsoleLibrary, Output formatting etc.

--- trunk/HGJ.ConsoleLib/ConsolePoint.cs
+++ trunk/HGJ.ConsoleLib/ConsolePoint.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace HGJ.ConsoleLib {
   public class ConsolePoint {
+    public ConsolePoint(int left, int top) {
+      X = left;
+      Y = top;
+    }
     public int X { get; set; } //left
     public int Y { get; set; } //top
   }
---
+++ trunk/HGJ.ConsoleLib/RegionDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HGJ.ConsoleLib {
+  public class RegionDictionary<TKey, TValue> : Dictionary<TKey, TValue> {
+    public event AddEventHandler AddEvent;
+    public event RemoveEventHandler RemoveEvent;
+
+    public void Add(TKey pKey, TValue pValue) {
+      if (AddEvent != null)
+        AddEvent(new AddEventArgs(pKey, pValue));
+      base.Add(pKey, pValue);
+    }
+    public void Remove(TKey pKey) {
+      if (RemoveEvent != null)
+        RemoveEvent(new RemoveEventArgs(pKey));
+      base.Remove(pKey);
+    }
+
+    public delegate void RemoveEventHandler(RemoveEventArgs pRemoveEventArgs);
+    public delegate void AddEventHandler(AddEventArgs pAddEventArgs);
+
+    public class AddEventArgs : EventArgs {
+      private TKey _key;
+      private TValue _value;
+
+      public AddEventArgs(TKey key, TValue value) {
+        _key = key;
+        _value = value;
+      }
+
+      public TKey Key {
+        get {
+          return _key;
+        }
+      }
+
+      public TValue Value {
+        get {
+          return _value;
+        }
+      }
+    }
+    public class RemoveEventArgs : EventArgs {
+      private TKey _key;
+
+      public RemoveEventArgs(TKey key) {
+        _key = key;
+      }
+
+      public TKey Key {
+        get {
+          return _key;
+        }
+      }
+    }
+  }
+}
