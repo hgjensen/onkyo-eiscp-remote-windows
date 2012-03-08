093be55ccc84d3f81c45f7588bf7faa153de3aab
hgj316@gmail.com <hgj316@gmail.com@b9641ce2-5edb-6cd8-cb65-f675c16494b0>
2012-03-08 19:28:00 +0000

    input-selection menu

--- OnkyoISCPlib/ISCPCmdAttrib.cs
+++ OnkyoISCPlib/ISCPCmdAttrib.cs
@@ -7,12 +7,18 @@ namespace OnkyoISCPlib {
   internal class ISCPCmdAttrib : Attribute {
     public string Name { get; set; }
     public string Regex { get; set; }
+    public string DisplayName { get; set; }
 
     public ISCPCmdAttrib(string name) {
       Name = name;
     }
-    public ISCPCmdAttrib(string name, string regex) {
-      Regex = regex;
+    //public ISCPCmdAttrib(string name, string regex) {
+    //  Name = name;
+    //  Regex = regex;
+    //}
+    public ISCPCmdAttrib(string name, string displayName) {
+      Name = name;
+      DisplayName = displayName;
     }
   }
 }
